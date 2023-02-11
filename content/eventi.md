cf9705358a2b41e19c51f80da1507847e811c6fb
Andrea Longo <andrea.longo.12@studenti.unipd.it>
2023-02-11 18:45:50 +0100

    first commit

---
+++ content/eventi.md
@@ -0,0 +1,15 @@
+---
+title: Elenco eventi
+date: 2019-03-17T19:31:20.591Z
+eventi:
+  - description: 'Evento1'
+    luogo: posizione1
+    date: data1
+  - description: 'Evento2'
+    luogo: posizione2
+    date: data2
+  - description: 'Evento3'
+    luogo: posizione3
+    date: data3
+---
+Qui puoi trovare tutti gli eventi/concerti:
---
+++ pages/eventi.js
@@ -0,0 +1,37 @@
+import Head from "next/head"
+import styles from '../styles/Home.module.css';
+import Link from "next/link";
+import { Component } from 'react'
+import { attributes, react as EventiContent } from '../content/eventi.md';
+
+export default class Eventi extends Component {
+    render() {
+        let{title, eventi} = attributes;
+        return(
+            <div className={styles.container}>
+                <Head>
+                    <title>The Acid Eye</title>
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+
+                <main>
+                    <h1 className={styles.title}>
+                    Eventi
+                    </h1>
+                    <Link href="/">Home Page</Link>
+                    <EventiContent />
+                    <ul>
+                        {eventi.map((evento, k) => (
+                            <li key={k}>
+                                <h2>{evento.description}</h2>
+                                <p>{evento.luogo}</p>
+                                <p>{evento.date}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </main>
+
+        </div>
+        );
+    }
+}
