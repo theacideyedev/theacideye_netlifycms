0658c16acea9fe19dcaec3797dab0ad778ba66e7
Nicola Cecchetto <nicola.cecchetto.01@gmail.com>
2024-03-14 21:49:13 +0100

    hotfix

--- pages/components/premain.js
+++ pages/components/premain.js
@@ -8,7 +8,7 @@ export default class PreMain extends Component {
         return (
             <>
             <div id="whatsapp-div">
-                <p>Entra nel nostro gruppo Whatsapp per suonare alla prossima</p>
+                <p>Entra nel nostro gruppo Whatsapp per suonare alla</p>
                 <p><strong>"The Acid Eye Blues Jam"</strong></p>
                 <a href='https://chat.whatsapp.com/FH3Fw7ebN4U4VF1QBRPmrY' target='_blank'><button id="whatsapp-button">Unisciti al gruppo</button></a>
             </div>
